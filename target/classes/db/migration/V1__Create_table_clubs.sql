0b0db0f8bb93f341a1f0ce6883251c8d8acae97f
Paweł Bogdan <pawel_bogdan@icloud.com>
2021-10-06 12:42:37 +0200

    Delete target directory

--- target/classes/db/migration/V1__Create_table_clubs.sql
+++
@@ -1,5 +0,0 @@
-create table clubs (
-    short_name varchar(20) primary key,
-    full_name varchar(50),
-    squad_value varchar (15)
-);
--- target/classes/db/migration/V2__Add_columns_updated_and_created.sql
+++
@@ -1,2 +0,0 @@
-alter table clubs add column created_on datetime null;
-alter table clubs add column updated_on datetime null;
